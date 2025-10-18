a9ea239bd91ae850be0c61ce35551c20064b6c9e
Rutvi Shah <shahrutvi020@gmail.com>
2025-10-18 09:11:15 +0530

    initial commit

---
+++ JAVASCRIPT/MathObject.js
@@ -0,0 +1,7 @@
+console.log("Math.PI:", Math.PI);
+console.log("Square root of 16:", Math.sqrt(16));
+console.log("Power:", Math.pow(2, 5));
+console.log("Random number (0–1):", Math.random());
+console.log("Rounded value:", Math.round(4.6));
+console.log("Ceil value:", Math.ceil(4.2));
+console.log("Floor value:", Math.floor(4.9));
---
+++ JAVASCRIPT/interaction.js
@@ -0,0 +1,14 @@
+// alert - shows a message
+alert("Hey Rutvii! Welcome to JavaScript ");
+
+// prompt - takes user input
+let name = prompt("What’s your name?");
+alert("Nice to meet you, " + name + "!");
+
+// confirm - asks for confirmation (OK or Cancel)
+let proceed = confirm("Do you want to continue learning?");
+if (proceed) {
+  alert("Yay! Let’s keep going ");
+} else {
+  alert("No worries! Come back when you’re ready ");
+}
