205060f97073212db9cb8ec8b2eabc2467fab0ce
Rutvi Shah <shahrutvi020@gmail.com>
2025-10-18 09:09:29 +0530

    operators practice

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,18 @@
+let a = 10, b = 3;
+
+// Arithmetic
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponent:", a ** b);
+
+// Assignment
+a += 5; // a = a + 5
+console.log("After += :", a);
+
+// Logical
+console.log(true && false); // false
+console.log(true || false); // true
+console.log(!false); // true
