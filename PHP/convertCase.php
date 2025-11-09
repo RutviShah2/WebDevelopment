1cbd2823cb436abfb0e8fe184ec4363048469a54
Rutvi Shah <shahrutvi020@gmail.com>
2025-11-09 09:25:59 +0530

    initial commit

---
+++ PHP/convertCase.php
@@ -0,0 +1,7 @@
+<?php
+$text = "php is interesting";
+echo "Uppercase: " . strtoupper($text) . "<br>";
+echo "Lowercase: " . strtolower($text) . "<br>";
+echo "First Letter Capital: " . ucfirst($text) . "<br>";
+echo "Each Word Capital: " . ucwords($text);
+?>
